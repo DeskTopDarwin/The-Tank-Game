8320cc6acfd7dd70b1a8f79d38eb4d9ac2a791fc
DeskTopDarwin <93140023+DeskTopDarwin@users.noreply.github.com>
2022-05-06 01:17:36 -0400

    Added cannon elevation to tank
    Doesnt quite work, i assume its due to the raycast trying to find a rigidbody within objects not containing any.
    does output very slim elevation.

--- The Tank Game/Assets/Scripts/TankControls/TurretMovement.cs
+++ The Tank Game/Assets/Scripts/TankControls/TurretMovement.cs
@@ -10,34 +10,62 @@ public class TurretMovement : MonoBehaviour
     public GameObject cannon;
     public Camera camera;
 
+    private float cameraAngle;
+    private float cannonElevation;
+    private Transform target;
+    private RaycastHit hit;
+
     
     void Start()
     {
         
     }
 
-    
     void Update()
     {
         setTurretRotation();
+        setElevatationOnTurret();
     }
 
     private void setTurretRotation()
     {
-        float angle = camera.transform.eulerAngles.y;
+        cameraAngle = camera.transform.eulerAngles.y;
         //Debug.Log("camera angle: " + camera.transform.rotation.y + ", " + Quaternion.Euler(0, angle, 0));
-        turret.transform.rotation = Quaternion.Euler(0, angle, 0);
+        turret.transform.rotation = Quaternion.Euler(0, cameraAngle, 0);
     }
 
     private void setElevatationOnTurret()
     {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            target = hit.transform;
+            //Debug.Log("hit something");
+        }
+        else
+        {
+            target = camera.transform;
+            //Debug.Log("camera trnaforgm thingy");
+        }
+
+        Vector3 cannonDirection = target.position - cannon.transform.position;
+
+        Debug.Log("direction of cannon: " + cannonDirection);
+
+        cannonElevation = Mathf.Sin(cannonDirection.y / cannonDirection.magnitude);
+        cannonElevation = Mathf.Clamp(cannonElevation, -15, 15);
+        Debug.Log("angle of the cannon: " + cannonElevation);
+        
+        cannon.transform.rotation = Quaternion.Euler(cannonElevation, cameraAngle, 0);
 
     }
 
-    private float angleBetweenGameObjects()
-    {
-        Vector2 x = new Vector2(camera.transform.position.x, camera.transform.position.z); 
-        Vector2 y = new Vector2(turret.transform.position.x, turret.transform.position.z);
-        return Vector2.Angle(y,x);
-    }
+    //private float angleBetweenGameObjects()
+    //{
+    //    Vector2 x = new Vector2(camera.transform.position.x, camera.transform.position.z); 
+    //    Vector2 y = new Vector2(turret.transform.position.x, turret.transform.position.z);
+    //    return Vector2.Angle(y,x);
+    //}
 }
