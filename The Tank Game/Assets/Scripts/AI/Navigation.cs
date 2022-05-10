0753f3ef5b0382599a164436ee98b6bf9ac944ac
DeskTopDarwin <francis.a.gaming@outlook.com>
2022-05-10 11:32:03 -0400

    Fixed map issues
    Map had massive offset and some prefabs were distorted.
    Should be fixed.

--- The Tank Game/Assets/Scripts/AI/Navigation.cs
+++ The Tank Game/Assets/Scripts/AI/Navigation.cs
@@ -5,31 +5,60 @@ using UnityEngine.AI;
 
 public class Navigation : MonoBehaviour
 {
-
     public Transform destination;
 
     NavMeshAgent navMeshAgent;
+    List<GameObject> capturePoints;
+
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        capturePoints = new List<GameObject>();
 
         if (navMeshAgent == null)
         {
             Debug.LogError("navMesh agent component not present in" + gameObject.name);
         }
 
-        navMeshAgent.SetDestination(destination.position);
+
+
+        //navMeshAgent.SetDestination(destination.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (destination == null)
+        if (false)
         {
             //get the nearest capture point   
         }
 
-
     }
+
+    //to be used externaly
+    public void StopToAttack()
+    {
+        SetNavDestination(transform.position);
+    }
+
+    private void GetClosesCapturePoint()
+    {
+        
+        
+        
+    }
+
+    private Vector3 OffSetDestination(Vector3 target)
+    {
+
+
+        return new Vector3();
+    }
+
+    private void SetNavDestination(Vector3 target)
+    {
+        navMeshAgent.SetDestination(target);
+    }
+
 }
