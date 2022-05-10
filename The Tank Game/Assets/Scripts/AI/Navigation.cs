454fa8f8ea8612888845ed12384f97f47dbe795b
DeskTopDarwin <francis.a.gaming@outlook.com>
2022-05-10 13:53:24 -0400

    progress with AI navigation script
    Just a checkpoint for the code I wrote in case of screw-ups.

--- The Tank Game/Assets/Scripts/AI/Navigation.cs
+++ The Tank Game/Assets/Scripts/AI/Navigation.cs
@@ -5,16 +5,22 @@ using UnityEngine.AI;
 
 public class Navigation : MonoBehaviour
 {
+
+
     public Transform destination;
 
     NavMeshAgent navMeshAgent;
-    List<GameObject> capturePoints;
+
+    private int offset = 4;
+    static List<GameObject> capturePoints;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        capturePoints = new List<GameObject>();
+        //capturePoints = new List<GameObject>();
+        if(capturePoints == null)
+            capturePoints = FindGameObjectsWithLayer(LayerMask.NameToLayer("CapturePoint"));
 
         if (navMeshAgent == null)
         {
@@ -22,8 +28,26 @@ public class Navigation : MonoBehaviour
         }
 
 
-
         //navMeshAgent.SetDestination(destination.position);
+    }
+
+    //finds all Capture points within the map
+    static List<GameObject> FindGameObjectsWithLayer(int layer) 
+    { 
+        GameObject[] goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        List<GameObject> goList = new List<GameObject>();
+
+        for (int i = 0; i < goArray.Length; i++)
+        { 
+            if (goArray[i].layer == layer) 
+                goList.Add(goArray[i]);
+            
+        } 
+        
+        if (goList.Count == 0)
+            return null;
+        
+        return goList; 
     }
 
     // Update is called once per frame
@@ -44,16 +68,34 @@ public class Navigation : MonoBehaviour
 
     private void GetClosesCapturePoint()
     {
+        Vector3 closesPoint; ;
+        float distance;
+
+        //servers both as default value and initialisation
+        closesPoint = capturePoints[0].transform.position;
+        distance = closesPoint.magnitude;
         
+        foreach (var point in capturePoints)
+        {
+            Vector3 betweenSelfAndPoint = transform.position - point.transform.position;
+            float tempValue = betweenSelfAndPoint.magnitude;
+
+            if (tempValue < distance)
+            {
+                distance = tempValue;
+                closesPoint = point.transform.position;
+            }
+        }
         
-        
+        closesPoint =  OffSetDestination(closesPoint);
+        SetNavDestination(closesPoint);
     }
 
     private Vector3 OffSetDestination(Vector3 target)
     {
 
 
-        return new Vector3();
+        return target;
     }
 
     private void SetNavDestination(Vector3 target)
